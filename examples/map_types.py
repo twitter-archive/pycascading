58f2d04483b8028379a4500f5503b40b0f0120d7
Gabor Szabo <gabor@twitter.com>
2012-04-06 09:29:50 -0700

    Trying different map types

---
+++ examples/map_types.py
@@ -0,0 +1,66 @@
+#
+# Copyright 2011 Twitter, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Example illustrating the different types of map operations."""
+
+
+from pycascading.helpers import *
+
+
+def main():
+    flow = Flow()
+    input = flow.source(Hfs(TextLine(), 'pycascading_data/town.txt'))
+
+    out_folder = 'pycascading_data/out/'
+
+    @udf(produces='word')
+    def decorated_udf(tuple):
+        for word in tuple.get('line').split():
+            yield [word]
+
+    def undecorated_udf(tuple):
+        for word in tuple.get('line').split():
+            yield [word]
+
+    # This will create an output with one field called 'word', as the UDF
+    # was declared with that
+    # In this case the swap swaps out the whole input tuple with the output
+    input | MapSwap(decorated_udf) | \
+    flow.tsv_sink(out_folder + 'decorated_udf')
+
+    # This will create an output with one unnamed field, but otherwise the
+    # same as the previous one
+    input | MapSwap(undecorated_udf) | \
+    flow.tsv_sink(out_folder + 'undecorated_udf')
+
+    # This will only replace the first ('line') field with the output of
+    # the map, but 'offset' will be retained
+    # Note that once we add an unnamed field, all field names will be lost
+    input | MapSwap(1, undecorated_udf) | \
+    flow.tsv_sink(out_folder + 'undecorated_udf_with_input_args')
+
+    # This will create one field only, 'word', just like the first example
+    input | MapSwap(undecorated_udf, 'word') | \
+    flow.tsv_sink(out_folder + 'undecorated_udf_with_output_fields')
+
+    # This one will add the new column, 'word', to all lines
+    input | MapAdd(decorated_udf) | \
+    flow.tsv_sink(out_folder + 'decorated_udf_all')
+
+    input | MapAdd(1, undecorated_udf, 'word') | \
+    flow.tsv_sink(out_folder + 'undecorated_udf_all')
+
+    flow.run(num_reducers=1)
+    return
--- python/pycascading/operators.py
+++ python/pycascading/operators.py
@@ -22,14 +22,15 @@
 * Rename fields: Rename
 """
 
-import itertools
+import itertools, inspect
 
 from cascading.tuple import Fields
 from cascading.operation import Identity
 import cascading.pipe.assembly.Rename
 import cascading.pipe.assembly.Shape
 
-from pycascading.pipe import Apply, SubAssembly, coerce_to_fields
+from pycascading.pipe import Apply, SubAssembly, coerce_to_fields, \
+DecoratedFunction
 from pycascading.decorators import udf
 
 
@@ -38,27 +39,50 @@ def _Map(output_selector, *args):
 
     The fields specified as input will be removed from the result.
     """
-    if len(args) == 2:
-        input_selector = Fields.ALL
-        (function, output_field) = args
+    if len(args) == 1:
+        (input_selector, function, output_field) = \
+        (Fields.ALL, args[0], Fields.UNKNOWN)
+    elif len(args) == 2:
+        if inspect.isfunction(args[0]) or isinstance(args[0], DecoratedFunction):
+            # The first argument is a UDF, the second are the output fields
+            (input_selector, function, output_field) = \
+            (Fields.ALL, args[0], args[1])
+        else:
+            (input_selector, function, output_field) = \
+            (args[0], args[1], Fields.UNKNOWN)
     elif len(args) == 3:
         (input_selector, function, output_field) = args
     else:
         raise Exception('Map needs to be called with 2 or 3 parameters')
-    df = udf(produces=output_field)(function)
+    if isinstance(function, DecoratedFunction):
+        # By default we take everything from the UDF's decorators
+        df = function
+    else:
+        df = udf(produces=output_field)(function)
     return Apply(input_selector, df, output_selector)
 
 
-def MapTo(*args):
+def MapSwap(*args):
     """Map the tuple, and remove the mapped fields, and add the new fields.
 
     This mapping replaces the fields mapped with the new fields that the
     mapping operation adds.
+
+    The number of arguments to this function is between 1 and 3:
+    * One argument: it's the map function. The output fields will be named
+      after the 'produces' parameter if the map function is decorated, or
+      will be Fields.UNKNOWN if it's not defined. Note that after UNKNOW field
+      names are introduced to the tuple, all the other field names are also
+      lost.
+    * Two arguments: it's either the input field selector and the map function,
+      or the map function and the output fields' names.
+    * Three arguments: they are interpreted as the input field selector, the
+      map function, and finally the output fields' names.
     """
     return _Map(Fields.SWAP, *args)
 
 
-def MapJoin(*args):
+def MapAdd(*args):
     """Map the defined fields (or all fields), and add the results to the tuple.
 
     Note that the new field names we are adding to the tuple cannot overlap
@@ -82,10 +106,21 @@ def Retain(*fields_to_keep):
 def _Discard(fields_to_discard):
     # In 2.0 there's a builtin function this, Discard
     # In 1.2 there is nothing for this
-    return Apply(fields_to_discard, Identity(), Fields.REPLACE)
+    raise Exception('Discard only works with Cascading 2.0')
 
 
-def Rename(fields_from, fields_to):
+def Rename(*args):
+    """Rename the fields to new names.
+
+    If only one argument (a list of names) is given, it is assumed that the
+    user wants to rename all the fields. If there are two arguments, the first
+    list is the set of fields to be renamed, and the second is a list of the
+    new names.
+    """
+    if len(args) == 1:
+        (fields_from, fields_to) = (Fields.ALL, args[0])
+    else:
+        (fields_from, fields_to) = (args[0], args[1])
     return SubAssembly(cascading.pipe.assembly.Rename, \
                        coerce_to_fields(fields_from), \
                        coerce_to_fields(fields_to))
